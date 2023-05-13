6813d2aaba34baf778597a3847e946f4f51eec40
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-13 18:33:44 +0100

    Revert "libvppdisplay: replace MAX_DECON_DMA_TYPE with IDMA_G0_S"
    Our decon header uses MAX_DECON_DMA_TYPE not IDMA_G0_S, so revert this commit
    
    This reverts commit 57aabf8882446a3904a5e2ff9615eab75e213868.

--- base/libhwc1/libvppdisplay/ExynosDisplay.cpp
+++ base/libhwc1/libvppdisplay/ExynosDisplay.cpp
@@ -2733,7 +2733,7 @@ void ExynosDisplay::determineBandwidthSupport(hwc_display_contents_1_t *contents
                     supportedInternalMPP->mState = MPP_STATE_ASSIGNED;
                     mLayerInfos[fbIndex]->mInternalMPP = supportedInternalMPP;
                     mLayerInfos[fbIndex]->mDmaType = getDeconDMAType(mLayerInfos[fbIndex]->mInternalMPP);
-                    if (mLayerInfos[fbIndex]->mDmaType >= IDMA_G0_S) {
+                    if (mLayerInfos[fbIndex]->mDmaType >= MAX_DECON_DMA_TYPE) {
                         ALOGE("getDeconDMAType with InternalMPP for FramebufferTarget failed (MPP type: %d, MPP index: %d)",
                                 mLayerInfos[fbIndex]->mInternalMPP->mType, mLayerInfos[fbIndex]->mInternalMPP->mIndex);
                         mLayerInfos[fbIndex]->mDmaType = 0;
